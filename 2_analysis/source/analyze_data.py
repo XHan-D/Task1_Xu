3ade12c18286dc675549347832a5e29dbddb7e97
Xu Han <xuhanx@stanford.edu>
2026-07-08 09:45:20 -0700

    #1 Add regression table with year-clustered standard errors

--- 2_analysis/source/analyze_data.py
+++ 2_analysis/source/analyze_data.py
@@ -13,17 +13,23 @@ def main():
 
 def regression_table(data): 
   reg_cty = sm.OLS.from_formula('displ ~ cty', data).fit()
-  print(reg_cty.summary())
 
   reg_hwy = sm.OLS.from_formula('displ ~ hwy', data).fit()
-  print(reg_hwy.summary())
 
   reg_hwy_cty = sm.OLS.from_formula('displ ~ cty + hwy', data).fit()
-  print(reg_hwy_cty.summary())
 
   latex_table = reg_hwy_cty.summary().as_latex()
   with open("../output/table_reg.tex", "w") as f:
     f.write(latex_table)
+
+  #Clustered by year
+  reg_hwy_cty2 = sm.OLS.from_formula('displ ~ cty + hwy', data).fit(cov_type="cluster", cov_kwds={"groups": data["year"]})
+  print(reg_hwy_cty2.summary())
+
+  latex_table2 = reg_hwy_cty2.summary().as_latex()
+  with open("../output/table_reg_cluster.tex", "w") as f:
+    f.write(latex_table2)
+
 
 def hwy_figure(data):
   plt.scatter(data['displ'], data['hwy'], c=data['year'])
