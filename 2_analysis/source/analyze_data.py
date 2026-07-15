650cb0b834b99abae76b488a99544744b872ca9d
Xu Han <xuhanx@stanford.edu>
2026-07-15 16:25:10 -0700

    Revert "#2 Use log highway fuel economy in analysis and slides"
    This reverts commit 53769016f47e1d64a55cdd2f6aeac7c035e560f4.

--- 2_analysis/source/analyze_data.py
+++ 2_analysis/source/analyze_data.py
@@ -32,9 +32,9 @@ def regression_table(data):
 
 
 def hwy_figure(data):
-  plt.scatter(data['displ'], np.log(data['hwy']), c=data['year'])
+  plt.scatter(data['displ'], data['hwy'], c=data['year'])
   plt.xlabel("Engine displacement (L)")
-  plt.ylabel("Log highway fuel economy (log mpg)")
+  plt.ylabel("Highway fuel economy (mpg)")
   plt.savefig("../output/figure_hwy.jpg")
 
 def city_figure(data):
